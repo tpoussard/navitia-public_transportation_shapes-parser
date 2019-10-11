5d82a6eaa532929428667fa1003afab616934239
timothee_poussard <timothee.poussard@yahoo.com>
2019-10-11 11:00:33 +0200

    refactorization with list comprehension and print with end='\r'

--- fr-idf/parser/navitia_parser_multiline_main.py
+++ fr-idf/parser/navitia_parser_multiline_main.py
@@ -16,26 +16,23 @@ with open('../ntfs/geometries.txt', 'r') as file:
         if index > 0:
             field = line.split(',', 1)
             geometry_id = field[0]
-            print('geometry_id', geometry_id)
+            print('geometry_id', geometry_id, end='\r')
             properties = {'geometry_id': geometry_id}
 
     # search for routeId matching geometry_id to fill properties
-            # test to factorize:
-            # properties = [obj for obj in geometry2line_id if obj['geometry_id'] == geometry_id]
-            for obj in geometry2line_id:
-                if obj['geometry_id'] == geometry_id:
-                    properties.update({
+            [properties.update({
                         'line_id': obj['line_id'],
                         'lineColor': obj['lineColor'],
-                        'lineTextColor': obj['lineTextColor']
-                    })
-                    break
-
+                        'lineTextColor': obj['lineTextColor']})
+                for obj in geometry2line_id
+                if obj['geometry_id'] == geometry_id
+            ]
+    # look up for mistakes
             if 'line_id' not in properties:  # check if lineId is missing
                 print('no corresponding lineId found for geometry_id:', geometry_id, properties)
             if properties['line_id'] == '':  # check if lineID is empty
                 notFoundIndex += 1
-        #  format shape data from CSV format to json format
+        #  format shape data from string format to json format
             #  remove parenthesis
             rawMultiLine = field[1].split('((')[-1]  # at the beginning
             rawMultiLine = rawMultiLine.split('))"') # at the end
@@ -58,14 +55,14 @@ with open('../ntfs/geometries.txt', 'r') as file:
             if len(properties) == 0:  # check if properties is missing
                 print('missing properties for geometry_id', geometry_id)
     # split and save when reaching 110 public transportation line object to avoid big files
-        # in order to be able to add them manually to mapbox for instance
+    # in order to be able to add them manually to mapbox for instance
             if index % 110 == 0:
                 write2file(arrayOfGeojsonItems, index, formerIndex)
 
-                formerIndex = index + 1
+                formerIndex = index + 1 # increment former index to write in the file name next time
                 if notFoundIndex != 0:  # check if there was missing shape
                     print('\nMissing lineIds =', notFoundIndex)
-                arrayOfGeojsonItems = []
+                arrayOfGeojsonItems = [] # empty list that has already been saved
                 notFoundIndex = 0
         index += 1
     #  save the rest of shapes at the end
